a67400a0b3528d3bc1882bde51d9837420eed371
Nicolas Conte <nicoconte1999@gmail.com>
2022-07-19 18:28:06 -0300

    transactions example

---
+++ TransactionsExecution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCrudNET.Examples
+{
+    public class TransactionsExecution
+    {
+        public void Test()
+        {
+            EasyCrud easyCrud = new EasyCrud();
+            easyCrud.SetSqlConnection("ConnectionString");
+
+            var success = easyCrud
+                .BeginTransaction((queries) =>
+                {
+                    queries.Add(("insert into your_table values (@scalarVariable1, @scalarVariable2, @scalarVariable3)", new
+                    {
+                        scalarVariable1 = "a",
+                        scalarVariable2 = "b",
+                        scalarVariable3 = "c"
+                    }));
+
+                    queries.Add(("insert into your_table values (@scalarVariable11, @scalarVariable22, @scalarVariable33)", new
+                    {
+                        scalarVariable11 = "a",
+                        scalarVariable22 = "b",
+                        scalarVariable33 = "c"
+                    }));
+
+                    queries.Add(("insert into your_tables values ('1', GETDATE(), GETDATE(), GETDATE())", null));
+                })
+                .Commit();
+
+        }
+    }
+}
